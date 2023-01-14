b0821b5505330861cd8b59b0ba0e13136042416d
Shahid Yusuf <shd.92yus@gmail.com>
2023-01-14 20:39:53 +0500

    shop

---
+++ multiply.js
@@ -0,0 +1,18 @@
+const num1 = Math.ceil(Math.random()*10);
+// console.log(num1)
+const num2 = Math.ceil(Math.random()*10);
+
+const questionEl = document.getElementById("question");
+const inputEl = document.getElementById("input")
+const formEl = document.getElementById("form")
+questionEl.innerText = `What is ${num1} multiply by ${num2}?`
+
+const correctAns = num1 * num2;
+
+formEl.addEventListener("submit", ()=> {
+    const userAns = inputEl.value 
+// })
+    console.log(userAns, typeof userAns);
+})
+
+
